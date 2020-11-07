41e526b2590e2f77863116c3fac90ee404181b20
Arthur Terozendi <arthurterozendi@gmail.com>
2020-11-07 16:37:50 -0300

    selection sort v1.2

--- Java POO/AlgoritmosDeOrdenação/SelectionSort/src/ERE/AlgoritmoDeOrdenacao/Leitura.java
+++ Java POO/AlgoritmosDeOrdenação/SelectionSort/src/ERE/AlgoritmoDeOrdenacao/Leitura.java
@@ -1,7 +1,41 @@
 package ERE.AlgoritmoDeOrdenacao;
 
-public class Leitura {
-    public Leitura(){
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class Leitura {
+    private File path;
+    public Leitura(){
+        path = new File("cotacaoEMBR3.csv");
+    }
+
+    public ArrayList<Float> ler() throws IOException {
+        ArrayList<Float> cotacoes = new ArrayList<>();
+        FileReader lerArquivo = new FileReader(path);
+        BufferedReader bLerArquivo = new BufferedReader(lerArquivo);
+
+        int i = 0;
+        while (bLerArquivo.ready()){
+            String linha;
+            if (i == 0) {
+                linha = bLerArquivo.readLine();
+            } else {
+                linha = bLerArquivo.readLine();
+                cotacoes.add(Float.valueOf(separarLinha(linha)));
+            }
+            i++;
+        }
+
+        return cotacoes;
+    }
+
+    private String separarLinha(String linha) {
+
+        String linhaSeparada[] = linha.split(";");
+        linha = linhaSeparada[1];
+        return linha;
     }
 }
--- Java POO/AlgoritmosDeOrdenação/SelectionSort/src/ERE/AlgoritmoDeOrdenacao/Main.java
+++ Java POO/AlgoritmosDeOrdenação/SelectionSort/src/ERE/AlgoritmoDeOrdenacao/Main.java
@@ -1,16 +1,19 @@
 package ERE.AlgoritmoDeOrdenacao;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
 
         Random rng = new Random();
-        int lista[] = new int[1000];
+        int lista[] = new int[10];
+        Leitura leitura = new Leitura();
 
-        for (int i = 0; i < lista.length; i++) {
+       /** for (int i = 0; i < lista.length; i++) {
             lista[i] = rng.nextInt(100);
         }
 
@@ -26,6 +29,24 @@ public class Main {
                 lista[i] = lista[menorIndex];
                 lista[menorIndex] = aux;
             }
+        }*/
+       ArrayList<Float> cotacao = leitura.ler();
+        System.out.println(cotacao);
+        for (int i = 0; i < cotacao.size(); i++) {
+            int menorIndex = i;
+            for (int j = i; j < cotacao.size(); j++) {
+                if (cotacao.get(menorIndex) > cotacao.get(j)) {
+                    menorIndex = j;
+                }
+            }
+            if (cotacao.get(i) > cotacao.get(menorIndex)){
+                float aux = cotacao.get(i);
+                cotacao.add(i, cotacao.get(menorIndex));
+                cotacao.add(menorIndex, aux);
+            }
         }
+
+
+       System.out.println(cotacao);
     }
 }
